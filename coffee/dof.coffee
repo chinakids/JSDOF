aa9ef224246009f39aa6bc8858e85c25fb250dd7
chinakids <494063570@qq.com>
2015-08-26 13:11:21 +0800

    ...

--- coffee/dof.coffee
+++ coffee/dof.coffee
@@ -2,11 +2,13 @@ factory = ($) ->
 	class dof
 		constructor: ->
 			@test = true
-			@start = 0.2
-			@end = 0.8
+			@startPoint = 0.2
+			@endPoint = 0.8
 			@zoom = 0.5
 			@speed = 10   #px/s
 			@spacing = 20  #px
+			@mainDom = '.dof'
+			@layerDom = '.dof-layer'
 
 		setOption : (option) ->
 			unless option
@@ -19,11 +21,33 @@ factory = ($) ->
 
 
 		delay : () ->
-			alert('初始化成功')
+			zAxis = ($(@mainDom).find(@layerDom).size() * @spacing)/(@endPoint - @startPoint)
+			@cacheData =
+				ww    : $(window).width()
+				wh    : $(window).height()
+				zAxis : zAxis
+			@initPonit =
+				x    : $(window).width()/2
+				y    : $(window).height()/2
+			$(@mainDom).css
+				'width' : @cacheData.ww
+				'height': @cacheData.wh
 
-		model : (offset) ->
+			@layer()
+
+		model : (Moffset = {left:0,top:0}) ->
+			Poffset =
+				left : offset.left > @cacheData.ww/2 ? offset.left - @cacheData.ww/2 : @cacheData.ww/2 - offset.left
+				top : offset.top > @cacheData.wh/2 ? offset.top - @cacheData.wh/2 : @cacheData.wh/2 - offset.top
+			Xtan = Poffset.left/@cacheData.zAxis
+			ytan = Poffset.top/@cacheData.zAxis
 
 		layer : () ->
+			$(@mainDom).find(@layerDom).each () ->
+				deepin = $(@).attr('dof-deepin')
+				console.log deepin
+				$(@).attr 'style','zindex:'+deepin * 100
+				return
 
 
 	window.dof = dof
--- scripts/dof.js
+++ scripts/dof.js
@@ -6,11 +6,13 @@
     dof = (function() {
       function dof() {
         this.test = true;
-        this.start = 0.2;
-        this.end = 0.8;
+        this.startPoint = 0.2;
+        this.endPoint = 0.8;
         this.zoom = 0.5;
         this.speed = 10;
         this.spacing = 20;
+        this.mainDom = '.dof';
+        this.layerDom = '.dof-layer';
       }
 
       dof.prototype.setOption = function(option) {
@@ -28,12 +30,52 @@
       };
 
       dof.prototype.delay = function() {
-        return alert('初始化成功');
+        var zAxis;
+        zAxis = ($(this.mainDom).find(this.layerDom).size() * this.spacing) / (this.endPoint - this.startPoint);
+        this.cacheData = {
+          ww: $(window).width(),
+          wh: $(window).height(),
+          zAxis: zAxis
+        };
+        this.initPonit = {
+          x: $(window).width() / 2,
+          y: $(window).height() / 2
+        };
+        $(this.mainDom).css({
+          'width': this.cacheData.ww,
+          'height': this.cacheData.wh
+        });
+        return this.layer();
       };
 
-      dof.prototype.model = function(offset) {};
+      dof.prototype.model = function(Moffset) {
+        var Poffset, Xtan, ytan, _ref, _ref1;
+        if (Moffset == null) {
+          Moffset = {
+            left: 0,
+            top: 0
+          };
+        }
+        Poffset = {
+          left: (_ref = offset.left > this.cacheData.ww / 2) != null ? _ref : offset.left - this.cacheData.ww / {
+            2: this.cacheData.ww / 2 - offset.left
+          },
+          top: (_ref1 = offset.top > this.cacheData.wh / 2) != null ? _ref1 : offset.top - this.cacheData.wh / {
+            2: this.cacheData.wh / 2 - offset.top
+          }
+        };
+        Xtan = Poffset.left / this.cacheData.zAxis;
+        return ytan = Poffset.top / this.cacheData.zAxis;
+      };
 
-      dof.prototype.layer = function() {};
+      dof.prototype.layer = function() {
+        return $(this.mainDom).find(this.layerDom).each(function() {
+          var deepin;
+          deepin = $(this).attr('dof-deepin');
+          console.log(deepin);
+          $(this).attr('style', 'zindex:' + deepin * 100);
+        });
+      };
 
       return dof;
 
